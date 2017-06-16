e6486c7a2fe2a0dcf9918d2457db29cf11704b40
Taylor <taylor@pasq.net>
2017-06-16 13:57:15 -0600

    working on remove duplicates

--- src/reducers/markerLocations.js
+++ src/reducers/markerLocations.js
@@ -8,6 +8,26 @@ const MarkerLocations = (state = initialState, action) => {
     case 'MARKER_SELECTED':
 
 markerArray.push(action.data.latlng)
+
+
+
+//WORK IN PROGRESS
+// let last_element = markerArray[markerArray.length - 1];
+// var index = markerArray.indexOf(action.data.latlng);
+// var lastIndef = markerArray.lastIndexOf(action.data.latlng);
+// console.log('lastIndex ' + lastIndef)
+// console.log(index + ' index')
+// if (lastIndef != index) {
+//     markerArray.splice(action.data.latlng, 1);
+// }
+
+// for(var i = markerArray.length-1; i--;){
+//   console.log(markerArray[i])
+// 	if (markerArray[i] === action.data.latlng){
+//     markerArray.splice(markerArray[i], );
+//   }
+// }
+
       return [markerArray]
     }
 
